a142b4e855dc49de490c11dac8f2bcf5e53ba6f1
Yuta Koshizawa <koher@koherent.org>
2023-04-22 21:24:32 +0900

    Initial Commit

---
+++ Maze4D/Maze4DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Maze4DApp.swift
+//  Maze4D
+//
+//  Created by Yuta Koshizawa on 2023/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Maze4DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
