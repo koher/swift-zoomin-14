ebb1f3814e95e585970fa80392dd123add6fcd6b
Yuta Koshizawa <koher@koherent.org>
2023-04-22 23:06:00 +0900

    Maze4DViewをCanvasで再実装

--- Maze4D/Maze4DView.swift
+++ Maze4D/Maze4DView.swift
@@ -4,22 +4,19 @@ struct Maze4DView: View {
     let maze: Maze4D
     let cellSize: CGFloat
     let currentPosition: Vector4D<Int>
-
+    
     var body: some View {
-        VStack(spacing: cellSize) {
-            ForEach(1..<(maze.size.w - 1), id: \.self) { w in
-                HStack(spacing: cellSize) {
-                    ForEach(1..<(maze.size.z - 1), id: \.self) { z in
-                        VStack(spacing: 0) {
-                            ForEach(0..<maze.size.y, id: \.self) { y in
-                                HStack(spacing: 0) {
-                                    ForEach(0..<maze.size.x, id: \.self) { x in
-                                        Rectangle()
-                                            .fill(cellColor(x: x, y: y, z: z, w: w))
-                                            .frame(width: cellSize, height: cellSize)
-                                    }
-                                }
-                            }
+        Canvas { context, size in
+            for w in (1..<(maze.size.w - 1)) {
+                for z in (1..<(maze.size.z - 1)) {
+                    for y in (0..<maze.size.y) {
+                        for x in (0..<maze.size.x) {
+                            let fillColor = cellColor(x: x, y: y, z: z, w: w)
+                            let rect = CGRect(x: CGFloat(x) * cellSize + CGFloat(z - 1) * (CGFloat(maze.size.x) * cellSize + cellSize),
+                                              y: CGFloat(y) * cellSize + CGFloat(w - 1) * (CGFloat(maze.size.y) * cellSize + cellSize),
+                                              width: cellSize,
+                                              height: cellSize)
+                            context.fill(Path(rect), with: .color(fillColor))
                         }
                     }
                 }
@@ -32,9 +29,9 @@ struct Maze4DView: View {
         if position == currentPosition {
             return .green
         }
-
+        
         let cell = maze[position]
-
+        
         switch cell {
         case .path, .start:
             return .white
